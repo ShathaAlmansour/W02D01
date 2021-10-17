a15e329e60c070022ce2d8149bb2d6e94548a880
ShathaAlmansour <>
2021-10-17 23:50:28 +0300

    added file

--- main.js
+++ main.js
@@ -45,35 +45,53 @@ const includeOf = function (string, character) {
 const addToArray = function (array, string) {
     array.push(string);
     return array;
-  };
+};
 
 const convertToString = function (array) {
     return array.join();
 };
-  
+
 const updateValue = function (array, index, value) {
     if (array.length < index) {
-      return "there is no element to updated in this index";
+        return "there is no element to updated in this index";
     } else {
-      array[index] = value;
-      return array;
+        array[index] = value;
+        return array;
     }
-  };
+};
 
 const onlyString = function (array) {
-    let n=[];
-    for(let i = 0; i < array.length; i++){
-        if (typeof array[i] == "string"){
-            n.push(array[i]); }}
-    return n };
+    let n = [];
+    for (let i = 0; i < array.length; i++) {
+        if (typeof array[i] == "string") {
+            n.push(array[i]);
+        }
+    }
+    return n
+};
 
-    
+const users = [["Jane", "123456"], ["admin", "abc123"]];
+const login = function (username, password) {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i][0] === username && users[i][1] === password) {
+            return "Login Successful";
+        } else
+            return "Login Failed";
+    }
+};
 
+const maximumNumber = function (numbers) {
+    let max = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    return max;
+};
 
+const reversString = function (string) {
+    const string = string.split()
+    console.log(string)
+}
 
-
-
-  
-  
-
-  
